d10cc1c385a01cdd3bf9dd0a2a5ada4a5b66c1f0
poculanshang <presheamae@gmail.com>
2025-12-04 08:52:01 +0800

    Add files via upload

---
+++ Poculan11.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Poculan11 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Poculan11
+{
+   public static void main(String [] args){
+       int [] arr = {10,20,30,40,50};
+       for(int i = 0; 1 < arr.length; i += 2)
+   
+   System.out.print(arr[i] + " ");
+    }
+}
