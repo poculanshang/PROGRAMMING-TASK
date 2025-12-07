f879aefde34a7c544131d056bfe87f4e4ec1b19a
poculanshang <presheamae@gmail.com>
2025-12-07 11:39:33 +0800

    Add files via upload

---
+++ poculan14.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class poculan14 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class poculan14
+{
+    public static void main(String [] args){
+        int [] arr = {1,2,3,4,5};
+        
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr [i] + " ");
+        }
+    }
+    }
